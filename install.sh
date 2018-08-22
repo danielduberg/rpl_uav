adaf970ee75ad1599f456a702c68146e823b1cdf
Daniel Duberg <danielduberg@gmail.com>
2018-08-22 16:51:27 +0200

    Updated install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Mavros, Mavlink, and PX4
+wstool init src
+sudo apt-get install python-catkin-tools python-rosinstall-generator -y
+wstool init ~/rpl_ws/src
+wstool update -t src -j4
+rosdep install --from-paths src --ignore-src -y
+source ./src/mavros/mavros/scripts/install_geographiclib_datasets.sh
